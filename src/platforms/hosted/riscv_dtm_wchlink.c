67689ed3d2759ff456c70bd43df07280524bbc1c
mean <fixounet@free.fr>
2023-07-16 17:04:19 +0200

    wchlink : fix init dmi vs debug

--- src/platforms/hosted/riscv_dtm_wchlink.c
+++ src/platforms/hosted/riscv_dtm_wchlink.c
@@ -62,25 +62,15 @@ uint8_t riscv_dtm_wchlink_handler()
 	}
 	dmi_bus->read = riscv_dtm_wchlink_dmi_read;
 	dmi_bus->write = riscv_dtm_wchlink_dmi_write;
+	dmi_bus->version = RISCV_DEBUG_0_13;
 	dmi_bus->designer_code =
 		JEP106_MANUFACTURER_WCH; /* fixme: WCHLINK deosn't seem to have a jep106 code assigned this is a dummy code */
+	
+	/* Call higher level code to discover the DMI bus */		
 	riscv_dmi_init(dmi_bus);
+	uint8_t c=dmi_bus->ref_count;
 	if (!dmi_bus->ref_count) {
-		free(dmi_bus);
-		return 0;
-	}
-
-	/* Call higher level code to discover the DMI bus */
-
-	/* If we failed to find any DMs or Harts, free the structure */
-
-	riscv_dm_s *dm = calloc(1, sizeof(*dm));
-	if (!dm) { /* calloc failed: heap exhaustion */
-		DEBUG_WARN("calloc: failed in %s\n", __func__);
-		free(dmi_bus); // wrong!
-		return 0;
-	}
-	dm->dmi_bus = dmi_bus;
-	dm->version = RISCV_DEBUG_0_13;
-	return dmi_bus->ref_count;
+		free(dmi_bus);		
+	}	
+	return c;	
 }
